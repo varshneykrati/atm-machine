9db038b4f38a94bb334c0b19068aba320d7094b9
Vaidehi Kalore <vaidehi.kalore@bajajfinserv.in>
2023-09-20 15:33:36 +0530

    vaidehi final commit

--- src/main/java/com/atm/atmmachine/AtmMachineApplication.java
+++ src/main/java/com/atm/atmmachine/AtmMachineApplication.java
@@ -69,9 +69,9 @@ public class AtmMachineApplication implements CommandLineRunner{
 		
 //Entering user registration detail form with the -> address  -----
 		Address address1 = new Address("405/8 Nai Basti","Firozabad",283203,"Uttar Pradesh");
-		UserRegistration userRegistration1 = new UserRegistration("Krati Varshney",LocalDate.now(),"+918178234554",962239611757L,"krativarshne@gmail.com","KVar@7777","KVar@7777",UserRegistrationApproval.Active,address1,null);
+		UserRegistration userRegistration1 = new UserRegistration("Krati Varshney",LocalDate.now(),"+919156191350",962239611757L,"krativarshne@gmail.com","KVar@7777","KVar@7777",UserRegistrationApproval.Active,address1,null);
 
-		UserRegistration userRegistration2 = new UserRegistration("Shivam",LocalDate.now(),"+918178234554",301577650820L,"shivam@gmail.com","shivam@7777","shivam@7777",UserRegistrationApproval.Active,new Address("415/8 New Market","Agra",283203,"Uttar Pradesh"),null);
+		UserRegistration userRegistration2 = new UserRegistration("Shivam",LocalDate.now(),"+919156191350",301577650820L,"shivam@gmail.com","shivam@7777","shivam@7777",UserRegistrationApproval.Active,new Address("415/8 New Market","Agra",283203,"Uttar Pradesh"),null);
 
 		UserRegistration userRegistration3 = new UserRegistration("Sidhi",LocalDate.now(),"+918178234554",767678355011L,"sidhi@gmail.com","sidhi@7777","sidhi@7777",UserRegistrationApproval.Inactive,new Address("415/8 Old Market","Agra",283203,"Madhya Pradesh"),null);
 
@@ -91,11 +91,11 @@ public class AtmMachineApplication implements CommandLineRunner{
 		
 		
 //Entering Card Detail of the user ->link with USER Registration
-		CardDetails carddetail1 = new CardDetails(new BigInteger("123412341234"),new BigInteger("7890789078907890"),456,LocalDate.now(),CardType.Silver,25000.0,CardStatus.Active,20000.0,3456,UserTotallyRegister.True,userRegistration2);
+		CardDetails carddetail1 = new CardDetails(new BigInteger("123412341234"),new BigInteger("7890789078907890"),456,LocalDate.now(),CardType.Silver,500.0,CardStatus.Active,2000.0,3456,UserTotallyRegister.True,userRegistration2);
 		 userRegistration2.setCardDetails(carddetail1);cardDetailsRepository.save(carddetail1); userRegistrationRepository.save(userRegistration2);
-		 CardDetails carddetail2 = new CardDetails(new BigInteger("123412344321"),new BigInteger("7890789078900965"),789,LocalDate.now(),CardType.Silver,25000.0,CardStatus.Active,2000.0,null,UserTotallyRegister.False,userRegistration1);
+		 CardDetails carddetail2 = new CardDetails(new BigInteger("123412344321"),new BigInteger("7890789078900965"),789,LocalDate.now(),CardType.Silver,500.0,CardStatus.Active,2000.0,7897,UserTotallyRegister.False,userRegistration1);
 		 userRegistration1.setCardDetails(carddetail2);cardDetailsRepository.save(carddetail2); userRegistrationRepository.save(userRegistration1);
-		 CardDetails carddetail3 = new CardDetails(new BigInteger("1234123456678"),new BigInteger("7890789078999890"),459,LocalDate.now(),CardType.Silver,25000.0,CardStatus.Active,2000.0,null,UserTotallyRegister.False,userRegistration3);
+		 CardDetails carddetail3 = new CardDetails(new BigInteger("1234123456678"),new BigInteger("7890789078999890"),459,LocalDate.now(),CardType.Silver,25000.0,CardStatus.Active,20000.0,null,UserTotallyRegister.False,userRegistration3);
 		 userRegistration3.setCardDetails(carddetail3);cardDetailsRepository.save(carddetail3); userRegistrationRepository.save(userRegistration3);
 		 CardDetails carddetail4 = new CardDetails(new BigInteger("123424561234"),new BigInteger("7890789008907890"),956,LocalDate.now(),CardType.Silver,25000.0,CardStatus.Active,2000.0,null,UserTotallyRegister.False,userRegistration4);
 		 userRegistration4.setCardDetails(carddetail4);cardDetailsRepository.save(carddetail4); userRegistrationRepository.save(userRegistration4);
